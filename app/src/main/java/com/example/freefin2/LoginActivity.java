d4439ffaaf501a27858cb7a203bd2388429ca4b8
silviapinedajimenez <spinedajimenez@csumb.edu>
2024-04-19 11:54:31 -0700

    Created login abd create accoiunt buttons and landining page activity_landing_page.xml

--- app/src/main/java/com/example/freefin2/LoginActivity.java
+++ app/src/main/java/com/example/freefin2/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.freefin2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -12,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText usernameEditText, passwordEditText;
     private Button loginButton;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,15 +24,18 @@ public class LoginActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.editTextPassword);
         loginButton = findViewById(R.id.buttonLogin);
 
-        loginButton.setOnClickListener(view -> {
-            String username = usernameEditText.getText().toString();
-            String password = passwordEditText.getText().toString();
-
-            // Replace "admin" and "admin" with your actual username and password logic
-            if ("admin".equals(username) && "admin".equals(password)) {
-                Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(MainActivity.this, "Invalid credentials", Toast.LENGTH_LONG).show();
-            }
-        });
+        loginButton.setOnClickListener(this::onClick);
     }
+
+    private void onClick(View view) {
+        String username = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        // Replace "admin" and "admin" with your actual username and password logic
+        if ("admin".equals(username) && "admin".equals(password)) {
+            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(LoginActivity.this, "Invalid credentials", Toast.LENGTH_LONG).show();
+        }
+    }
+}
